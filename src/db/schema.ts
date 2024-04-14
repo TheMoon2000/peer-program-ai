27fb0d2130ee9af685710e7c378065a8013bfda2
cyu60 <chinatchinat123@gmail.com>
2024-04-14 14:19:23 -0700

    update to have the meeting id nullable

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -25,12 +25,25 @@ export const rooms = pgTable("rooms", {
   codeState: text("code_state").notNull(),
   question: text("question").notNull(),
   token: text("token").notNull(),
+  meetingId: text("meeting_id"),
   terminalId: integer("terminal_id").notNull(), // Unique terminal ID for each room
   full: boolean("full").notNull().default(false), // Indicates if the room is full
   created: timestamp("created").notNull(), // created time for the room
   // roomId: uuid("room_id")
   //   .notNull()
   //   .references(() => rooms.id), // Foreign key linking to Rooms
+});
+
+export const chat = pgTable("chat", {
+  messageId: uuid("message_id").primaryKey(), // Unique identifier for each message
+  roomId: uuid("room_id")
+    .references(() => rooms.id, { onDelete: "cascade" })
+    .notNull(), // Foreign key linking to Rooms table
+  userId: uuid("user_id")
+    .references(() => users.userId, { onDelete: "cascade" })
+    .notNull(), // Foreign key linking to Users table
+  messageContent: text("message_content").notNull(), // The content of the message
+  timeCreated: timestamp("time_created").notNull(), // Timestamp of when the message was created
 });
 
 // Define the Users table
