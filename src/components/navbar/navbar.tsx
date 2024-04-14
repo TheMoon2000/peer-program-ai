5625618046350adfa121f18e73f95db11c3f2036
cyu60 <chinatchinat123@gmail.com>
2024-04-14 02:23:35 -0700

    update navbar

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <div className="bg-gray-800 p-8">
+      <div className="md:flex md:items-center md:justify-between">
+        <div className="min-w-0 flex-1">
+          <h2 className="text-2xl font-bold leading-7 text-white sm:truncate sm:text-3xl sm:tracking-tight">
+            Peer Program
+          </h2>
+        </div>
+        <div className="mt-4 flex md:ml-4 md:mt-0">
+          {/* <Link
+            href={"/"}
+            //   type="button"
+            className="inline-flex items-center rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-white/20"
+          >
+            Leave
+          </Link> */}
+          <div className="flex -space-x-2 mx-3">
+            {/* <div className="flex -space-x-2 overflow-hidden"> */}
+            {/* <img
+              className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+              src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+            <img
+              className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+              src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            /> */}
+            <img
+              className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+              src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
+              alt=""
+            />
+            <img
+              className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+          </div>
+          <button
+            type="button"
+            className="ml-3 inline-flex items-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+          >
+            Run
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
